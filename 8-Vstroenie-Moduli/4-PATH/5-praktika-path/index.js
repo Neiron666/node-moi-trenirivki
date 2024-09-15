4d91f5569e2c54eaba57bcd1782d68bafb5ad780
Neiron666 <neiron666@gmail.com>
2024-09-15 11:27:56 +0300

    15-09-2024

---
+++ 8-Vstroenie-Moduli/4-PATH/5-praktika-path/index.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+const filePeth = "/Users/bogdan/Desctop/node/index.js";
+const text = "/Users/bogdan/Desctop/node/index.js";
+
+console.log(path.isAbsolute(filePeth));
